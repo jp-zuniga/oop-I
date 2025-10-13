af8fc74aefc400bbd71580728d7433cd485d5afe
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-10-13 16:40:56 -0600

    avanzar jpa

--- jpa-demo/src/main/java/Main.java
+++ jpa-demo/src/main/java/Main.java
@@ -8,6 +8,12 @@ public class Main {
 
     public static void main(String[] args) {
         IDAOSucursal dao = new ImplDAOSucursal();
+        Sucursal entity = new Sucursal();
+        entity.setId(4);
+        entity.setNombre("Jinotega");
+        entity.setDescripcion("Sucursal de Jinotega");
+        dao.insert(entity);
+
         List<Sucursal> lista = dao.getAll("Sucursal.All");
         lista.forEach(System.out::println);
     }
--- jpa-demo/src/main/java/service/ImplDAOSucursal.java
+++ jpa-demo/src/main/java/service/ImplDAOSucursal.java
@@ -9,17 +9,27 @@ import java.util.List;
 public class ImplDAOSucursal implements IDAOSucursal {
     @Override
     public List<Sucursal> getAll(String nameQuery) {
-        EntityManager em = EntityManagerAdmin.getInstance();
-        TypedQuery<Sucursal> query = em.createNamedQuery(nameQuery, Sucursal.class);
-        return query.getResultList();
+        try (EntityManager em = EntityManagerAdmin.getInstance()) {
+            TypedQuery<Sucursal> query = em.createNamedQuery(nameQuery, Sucursal.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Override
     public void insert(Sucursal entity) {
-        EntityManager em = EntityManagerAdmin.getInstance();
-        em.getTransaction().begin();
-        em.persist(entity);
-        em.flush();
-        em.getTransaction().commit();
+        try (EntityManager em = EntityManagerAdmin.getInstance()) {
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.flush();
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 }
